dff432cfdd78ce87f1c502a8cd5822fb22c56ec8
Elijah Mondero <elijahmondero@gmail.com>
2025-05-18 00:25:17 +1200

    fix: update alias for '@' in Vite configuration to use path.resolve

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { plugin as markdown } from 'vite-plugin-markdown';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react(), markdown()],
   resolve: {
     alias: {
-      '@': '/',
+      '@': path.resolve(__dirname, './'),
       // Polyfill Node.js modules
       process: 'process/browser',
       buffer: 'buffer/',
